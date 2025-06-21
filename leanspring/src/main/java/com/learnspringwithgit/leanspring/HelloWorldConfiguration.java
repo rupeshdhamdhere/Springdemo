077a2b77a384ce035e3bb1a6a4cc7a42e94fc162
rupeshdhamdhere <rupeshdhamdhere04@gmail.com>
2025-06-21 15:45:22 +0530

    commited successfully

--- leanspring/src/main/java/com/learnspringwithgit/leanspring/HelloWorldConfiguration.java
+++ leanspring/src/main/java/com/learnspringwithgit/leanspring/HelloWorldConfiguration.java
@@ -1,9 +1,14 @@
 package com.learnspringwithgit.leanspring;
-
+import org.springframework.context.annotation.Configuration;
+@Configuration
 public class HelloWorldConfiguration {
 
+
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Now we will create our first Bean
+		// To create Bean we need keword As Bean 
+		
 
 	}
 
