720883ab13cf2179c23db3a707e337765529c664
rupeshdhamdhere <rupeshdhamdhere04@gmail.com>
2025-06-21 12:03:42 +0530

    New files added to learn and configure and to create Bean

---
+++ leanspring/src/main/java/com/learnspringwithgit/leanspring/App01BasicGaming.java
@@ -0,0 +1,10 @@
+package com.learnspringwithgit.leanspring;
+
+public class App01BasicGaming {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ leanspring/src/main/java/com/learnspringwithgit/leanspring/App02HelloWorldSpring.java
@@ -0,0 +1,10 @@
+package com.learnspringwithgit.leanspring;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
