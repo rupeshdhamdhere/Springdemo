248da9f6f65b9b99d89048f5027bb0f52ccff160
rupeshdhamdhere <rupeshdhamdhere04@gmail.com>
2025-06-21 16:04:34 +0530

    the Bean is created and fetched succesfully

--- leanspring/src/main/java/com/learnspringwithgit/leanspring/App02HelloWorldSpring.java
+++ leanspring/src/main/java/com/learnspringwithgit/leanspring/App02HelloWorldSpring.java
@@ -1,10 +1,16 @@
 package com.learnspringwithgit.leanspring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+    //here we will launch a spring context 
+	// we will use annotation config application context class for that and will fetch the bean created in the configuration file
+	// as follows
+	//And we will get that beat by getBean method and will print it;
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		System.out.println(context.getBean("name"));
 	}
 
 }
--- leanspring/src/main/java/com/learnspringwithgit/leanspring/HelloWorldConfiguration.java
+++ leanspring/src/main/java/com/learnspringwithgit/leanspring/HelloWorldConfiguration.java
@@ -1,15 +1,14 @@
 package com.learnspringwithgit.leanspring;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 @Configuration
 public class HelloWorldConfiguration {
-
-
-
-	public static void main(String[] args) {
+	
 		// Now we will create our first Bean
-		// To create Bean we need keword As Bean 
-		
-
+		// To create Bean we need keword As Bean
+        @Bean
+		public String name() {
+			return "Rupesh";
 	}
 
 }
