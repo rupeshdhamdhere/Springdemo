1915ed2399f7270a33cd534ddb63b03be8a4a246
rupeshdhamdhere <rupeshdhamdhere04@gmail.com>
2025-06-21 11:58:56 +0530

    SpringProjectadded

---
+++ leanspring/src/main/java/com/learnspringwithgit/leanspring/LeanspringwithgitApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringwithgit.leanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanspringwithgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanspringwithgitApplication.class, args);
+	}
+
+}
---
+++ leanspring/src/test/java/com/learnspringwithgit/leanspring/LeanspringwithgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringwithgit.leanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanspringwithgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
